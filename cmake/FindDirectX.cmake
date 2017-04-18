a035081bca3eef7f09e371401bfbe0aecd24e6e9
Tobias Guenther <tobias@isg.cs.uni-magdeburg.de>
2017-04-18 18:52:23 +0200

    adjusted DirectX search for latest Win SDK (10.0.15063.0)

--- cmake/FindDirectX.cmake
+++ cmake/FindDirectX.cmake
@@ -1,8 +1,6 @@
 # - try to find DirectX include dirs and libraries
 
-
 if (${CMAKE_SYSTEM_NAME} STREQUAL "Windows")
-
 
 	if (CMAKE_CL_64)
 		set (DirectX_ARCHITECTURE x64)
@@ -17,7 +15,7 @@ if (${CMAKE_SYSTEM_NAME} STREQUAL "Windows")
 	if (DEFINED MSVC_VERSION AND NOT ${MSVC_VERSION} LESS 1700)
 	
 		# iterate the Windows SDK versions
-		foreach(winsdk_version  10.0.14393.0  10.0.10586.0  10.0.10240.0  10.0.10150.0)
+		foreach(winsdk_version  10.0.15063.0  10.0.14393.0  10.0.10586.0  10.0.10240.0  10.0.10150.0)
 		
 			find_path (WIN10_SDK_ROOT_DIR
 				Include/${winsdk_version}/um/windows.h
@@ -29,7 +27,7 @@ if (${CMAKE_SYSTEM_NAME} STREQUAL "Windows")
 			if (WIN10_SDK_ROOT_DIR)
 				set (DirectX_INC_SEARCH_PATH "${WIN10_SDK_ROOT_DIR}/Include/${winsdk_version}/um" "${WIN10_SDK_ROOT_DIR}/Include/shared")
 				set (DirectX_LIB_SEARCH_PATH "${WIN10_SDK_ROOT_DIR}/Lib/${winsdk_version}/um/${DirectX_ARCHITECTURE}")
-				set (DirectX_BIN_SEARCH_PATH "${WIN10_SDK_ROOT_DIR}/bin/x86")
+				set (DirectX_BIN_SEARCH_PATH "${WIN10_SDK_ROOT_DIR}/bin/x86" "${WIN10_SDK_ROOT_DIR}/bin/${winsdk_version}/x86")
 				break()
 			endif()
 		
@@ -61,7 +59,6 @@ if (${CMAKE_SYSTEM_NAME} STREQUAL "Windows")
 		endif ()
 	endif ()
 
-
 	find_path (DirectX_D3D11_1_INCLUDE_DIR d3d11_1.h
 		PATHS ${DirectX_INC_SEARCH_PATH}
 		DOC "The directory where d3d11_1.h resides")
@@ -70,16 +67,15 @@ if (${CMAKE_SYSTEM_NAME} STREQUAL "Windows")
 		set (DirectX_D3D11_1_FOUND 1)
 	endif ()
 
-
 	find_program (DirectX_FXC_EXECUTABLE fxc
 		PATHS ${DirectX_BIN_SEARCH_PATH}
 		DOC "Path to fxc.exe executable.")
     
-  find_library (DirectX_D3DCOMPILER_LIBRARY d3dcompiler
+	find_library (DirectX_D3DCOMPILER_LIBRARY d3dcompiler
 		PATHS ${DirectX_LIB_SEARCH_PATH}
 		DOC "The directory where d3dcompiler resides")
     
-  find_library (DirectX_DXGI_LIBRARY dxgi
+	find_library (DirectX_DXGI_LIBRARY dxgi
 		PATHS ${DirectX_LIB_SEARCH_PATH}
 		DOC "The directory where dxgi resides")
     
@@ -98,9 +94,7 @@ if (${CMAKE_SYSTEM_NAME} STREQUAL "Windows")
 		DirectX_D3D11_1_INCLUDE_DIR
 	)
 
-
 endif ()
-
 
 mark_as_advanced (
 	DirectX_D3D_FOUND
@@ -109,6 +103,5 @@ mark_as_advanced (
 	DirectX_D3DX11_FOUND
 	DirectX_D3D11_1_FOUND
 )
-
 
 # vim:set sw=4 ts=4 noet:
